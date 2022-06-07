449f1fb8718ed31cae36da6f532badce17d78342
Bodikwa <85231717+Bodikwa@users.noreply.github.com>
2022-06-07 14:34:33 +0200

    Add project files.

---
+++ OwensPedPedWebApplication/Controllers/CompanyController.cs
@@ -0,0 +1,120 @@
+﻿using BackEndServices.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OwensPedPed.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OwensPedPedWebApplication.Controllers
+{
+    public class CompanyController : Controller
+    {
+
+        private readonly ICompanyRepository _companyRepository;
+        private readonly ICountryRepository _countryRepository;
+
+
+        public CompanyController(ICompanyRepository companyRepository, ICountryRepository countryRepository)
+        {
+            _companyRepository = companyRepository;
+            _countryRepository = countryRepository;
+        }
+        // GET: CompanyController
+        public ActionResult Index()
+        {
+            return View( _companyRepository.GetAllCompanies());
+        }
+
+        // GET: CompanyController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(_companyRepository.GetCompanyById(id));
+        }
+
+        // GET: CompanyController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CompanyController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] Company company)
+        {
+            try
+            {
+                _companyRepository.InsertCompany(company);
+                _companyRepository.SaveCompany();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private void GetProvenceViewbag()
+        {
+            var countries = _countryRepository.GetAllCountries().OrderBy(x => x.CountryName);
+
+            List<SelectListItem> countryOption = new List<SelectListItem>();
+
+            foreach (var c in countries)
+            {
+                countryOption.Add(new SelectListItem(c.CountryName, c.CountryId.ToString()));
+            }
+
+            ViewBag.countryViewbag = countryOption;
+        }
+
+        // GET: CompanyController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(_companyRepository.GetCompanyById(id));
+        }
+
+        // POST: CompanyController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([FromForm] Company company)
+        {
+            try
+            {
+                _companyRepository.UpdateCompany(company);
+                _companyRepository.SaveCompany();
+;                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CompanyController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(_companyRepository.GetCompanyById(id));
+        }
+
+        // POST: CompanyController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete([FromForm] Company company)
+        {
+            try
+            {
+                _companyRepository.DeleteCompany(company.CompanyId);
+                _companyRepository.SaveCompany();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ OwensPedPedWebApplication/Controllers/ProvenceController.cs
@@ -0,0 +1,98 @@
+﻿using BackEndServices.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OwensPedPedWebApplication.Controllers
+{
+    public class ProvenceController : Controller
+    {
+
+
+
+        private readonly IProvenceRepository _provenceRepository;
+
+
+        public ProvenceController(IProvenceRepository provenceRepository)
+        {
+            _provenceRepository = provenceRepository;
+        }
+        // GET: ProvenceController
+        public ActionResult Index()
+        {
+            return View(_provenceRepository.GetAllProvences());
+        }
+
+        // GET: ProvenceController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProvenceController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProvenceController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProvenceController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProvenceController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProvenceController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProvenceController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
